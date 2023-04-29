bfa97800e320952c43f619829c85cbeea52afa67
lin-835 <78547547+lin-835@users.noreply.github.com>
2023-04-29 16:22:15 +0800

    Initial commit

---
+++ shop_utils/shop_web_utils/src/main/java/com/atguigu/config/FooConfiguration.java
@@ -0,0 +1,12 @@
+package com.atguigu.config;
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FooConfiguration {
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
